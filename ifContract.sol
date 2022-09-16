d18c436be512859983560b9cbd3dd1c94afba6a4
0xdaebak <abw2912@gmail.com>
2022-09-16 11:25:33 +0530

    Create ifContract.sol

---
+++ ifContract.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7;
+
+
+//By : 0xdaebak
+//https://github.com/0xdaebak
+
+
+contract ifContract{
+
+
+function isContract(address addr) public view returns (bool) {
+  uint size;
+  assembly { size := extcodesize(addr) }
+  return size > 0;
+}
+
+
+
+
+}
+
